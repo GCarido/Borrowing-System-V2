606676e43b2dc58b35aea2322d5fa8383bd33017
Gerald Carido <gerald.carido@cit.edu>
2024-04-10 02:17:34 +0800

    Fix AdminAccountManager UI and backend logic

---
+++ BorrowingSystemV2/BorrowingSystemV2/AdminAccManagementPanels/AdminList.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace BorrowingSystemV2.AdminAccManagementPanels
+{
+    partial class AdminList
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.deleteBTN = new System.Windows.Forms.Button();
+            this.updateBTN = new System.Windows.Forms.Button();
+            this.createBTN = new System.Windows.Forms.Button();
+            this.adminIDTxtbx = new System.Windows.Forms.TextBox();
+            this.AfirstnameTxtbx = new System.Windows.Forms.TextBox();
+            this.AusernameTxtbx = new System.Windows.Forms.TextBox();
+            this.ApositionTxtbx = new System.Windows.Forms.TextBox();
+            this.AlastnameTxtbx = new System.Windows.Forms.TextBox();
+            this.ApasswordTxtbx = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // deleteBTN
+            // 
+            this.deleteBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(215)))), ((int)(((byte)(174)))));
+            this.deleteBTN.FlatAppearance.BorderSize = 0;
+            this.deleteBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.deleteBTN.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Bold);
+            this.deleteBTN.Location = new System.Drawing.Point(339, 360);
+            this.deleteBTN.Name = "deleteBTN";
+            this.deleteBTN.Size = new System.Drawing.Size(134, 57);
+            this.deleteBTN.TabIndex = 6;
+            this.deleteBTN.Text = "Delete";
+            this.deleteBTN.UseVisualStyleBackColor = false;
+            // 
+            // updateBTN
+            // 
+            this.updateBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(215)))), ((int)(((byte)(174)))));
+            this.updateBTN.FlatAppearance.BorderSize = 0;
+            this.updateBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.updateBTN.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Bold);
+            this.updateBTN.Location = new System.Drawing.Point(189, 360);
+            this.updateBTN.Name = "updateBTN";
+            this.updateBTN.Size = new System.Drawing.Size(134, 57);
+            this.updateBTN.TabIndex = 5;
+            this.updateBTN.Text = "Update";
+            this.updateBTN.UseVisualStyleBackColor = false;
+            // 
+            // createBTN
+            // 
+            this.createBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(215)))), ((int)(((byte)(174)))));
+            this.createBTN.FlatAppearance.BorderSize = 0;
+            this.createBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.createBTN.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Bold);
+            this.createBTN.Location = new System.Drawing.Point(33, 360);
+            this.createBTN.Name = "createBTN";
+            this.createBTN.Size = new System.Drawing.Size(134, 57);
+            this.createBTN.TabIndex = 4;
+            this.createBTN.Text = "Create";
+            this.createBTN.UseVisualStyleBackColor = false;
+            // 
+            // adminIDTxtbx
+            // 
+            this.adminIDTxtbx.BackColor = System.Drawing.Color.White;
+            this.adminIDTxtbx.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.adminIDTxtbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adminIDTxtbx.Location = new System.Drawing.Point(24, 56);
+            this.adminIDTxtbx.Multiline = true;
+            this.adminIDTxtbx.Name = "adminIDTxtbx";
+            this.adminIDTxtbx.Size = new System.Drawing.Size(202, 36);
+            this.adminIDTxtbx.TabIndex = 12;
+            // 
+            // AfirstnameTxtbx
+            // 
+            this.AfirstnameTxtbx.BackColor = System.Drawing.Color.White;
+            this.AfirstnameTxtbx.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.AfirstnameTxtbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AfirstnameTxtbx.Location = new System.Drawing.Point(24, 141);
+            this.AfirstnameTxtbx.Multiline = true;
+            this.AfirstnameTxtbx.Name = "AfirstnameTxtbx";
+            this.AfirstnameTxtbx.Size = new System.Drawing.Size(202, 36);
+            this.AfirstnameTxtbx.TabIndex = 13;
+            // 
+            // AusernameTxtbx
+            // 
+            this.AusernameTxtbx.BackColor = System.Drawing.Color.White;
+            this.AusernameTxtbx.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.AusernameTxtbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AusernameTxtbx.Location = new System.Drawing.Point(24, 225);
+            this.AusernameTxtbx.Multiline = true;
+            this.AusernameTxtbx.Name = "AusernameTxtbx";
+            this.AusernameTxtbx.Size = new System.Drawing.Size(202, 36);
+            this.AusernameTxtbx.TabIndex = 14;
+            // 
+            // ApositionTxtbx
+            // 
+            this.ApositionTxtbx.BackColor = System.Drawing.Color.White;
+            this.ApositionTxtbx.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ApositionTxtbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ApositionTxtbx.Location = new System.Drawing.Point(281, 56);
+            this.ApositionTxtbx.Multiline = true;
+            this.ApositionTxtbx.Name = "ApositionTxtbx";
+            this.ApositionTxtbx.Size = new System.Drawing.Size(202, 36);
+            this.ApositionTxtbx.TabIndex = 15;
+            // 
+            // AlastnameTxtbx
+            // 
+            this.AlastnameTxtbx.BackColor = System.Drawing.Color.White;
+            this.AlastnameTxtbx.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.AlastnameTxtbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AlastnameTxtbx.Location = new System.Drawing.Point(281, 141);
+            this.AlastnameTxtbx.Multiline = true;
+            this.AlastnameTxtbx.Name = "AlastnameTxtbx";
+            this.AlastnameTxtbx.Size = new System.Drawing.Size(202, 36);
+            this.AlastnameTxtbx.TabIndex = 16;
+            // 
+            // ApasswordTxtbx
+            // 
+            this.ApasswordTxtbx.BackColor = System.Drawing.Color.White;
+            this.ApasswordTxtbx.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ApasswordTxtbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ApasswordTxtbx.Location = new System.Drawing.Point(281, 225);
+            this.ApasswordTxtbx.Multiline = true;
+            this.ApasswordTxtbx.Name = "ApasswordTxtbx";
+            this.ApasswordTxtbx.Size = new System.Drawing.Size(202, 36);
+            this.ApasswordTxtbx.TabIndex = 17;
+            // 
+            // AdminList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(199)))), ((int)(((byte)(230)))));
+            this.BackgroundImage = global::BorrowingSystemV2.Properties.Resources.admininfo_icon;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Controls.Add(this.ApasswordTxtbx);
+            this.Controls.Add(this.AlastnameTxtbx);
+            this.Controls.Add(this.ApositionTxtbx);
+            this.Controls.Add(this.AusernameTxtbx);
+            this.Controls.Add(this.AfirstnameTxtbx);
+            this.Controls.Add(this.adminIDTxtbx);
+            this.Controls.Add(this.deleteBTN);
+            this.Controls.Add(this.updateBTN);
+            this.Controls.Add(this.createBTN);
+            this.DoubleBuffered = true;
+            this.Name = "AdminList";
+            this.Size = new System.Drawing.Size(508, 450);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button deleteBTN;
+        private System.Windows.Forms.Button updateBTN;
+        private System.Windows.Forms.Button createBTN;
+        private System.Windows.Forms.TextBox adminIDTxtbx;
+        private System.Windows.Forms.TextBox AfirstnameTxtbx;
+        private System.Windows.Forms.TextBox AusernameTxtbx;
+        private System.Windows.Forms.TextBox ApositionTxtbx;
+        private System.Windows.Forms.TextBox AlastnameTxtbx;
+        private System.Windows.Forms.TextBox ApasswordTxtbx;
+    }
+}
