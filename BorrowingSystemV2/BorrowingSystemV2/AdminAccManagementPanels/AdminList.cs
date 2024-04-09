9449570842118f099338eb5ded68d4864b4284bd
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-04-10 03:21:16 +0800

    Fix AdminAccountManagement UI and backend logic

---
+++ BorrowingSystemV2/BorrowingSystemV2/AdminAccManagementPanels/AdminList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BorrowingSystemV2.AdminAccManagementPanels
+{
+    public partial class AdminList : UserControl
+    {
+        public AdminList()
+        {
+            InitializeComponent();
+        }
+        public void UpdateAdminInfo()
+        {
+            adminIDTxtbx.Text = AdminAccountManangement.adminID;
+            AfirstnameTxtbx.Text = AdminAccountManangement.adminFName;
+            AlastnameTxtbx.Text = AdminAccountManangement.adminLName;
+            AusernameTxtbx.Text = AdminAccountManangement.adminUserName;
+            ApasswordTxtbx.Text = AdminAccountManangement.adminPassword;
+            ApositionTxtbx.Text = AdminAccountManangement.adminPosition;
+
+        }
+    }
+}
